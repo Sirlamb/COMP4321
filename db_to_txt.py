f0eeea7eca6995ff5d60bbd9602ffe006fcdaf51
Blaze342 <ykysaivto@gmail.com>
2025-05-04 02:10:49 +0800

    Update db_to_txt.py

--- db_to_txt.py
+++ db_to_txt.py
@@ -18,12 +18,12 @@ with open('spider_result.txt', 'w', encoding='utf-8') as file:
         file.write(date + ', ' + str(size) + '\n')
 
         # keyword info
-        cursor.execute(f'SELECT * FROM index_table WHERE page_id = {link_id}')
+        cursor.execute(f'SELECT * FROM forward_idx WHERE page_id = {link_id}')
         rows = cursor.fetchall()
         rows = sorted(rows, key=lambda x: x[2], reverse=True)
         used_word_id = []
         row_num = 0
-        while len(used_word_id) < 10 and row_num < len(rows):
+        while len(used_word_id) < 100 and row_num < len(rows):
             _, keyword_id, frequency, posiiton = rows[row_num]
             if keyword_id not in used_word_id:
                 used_word_id.append(keyword_id)

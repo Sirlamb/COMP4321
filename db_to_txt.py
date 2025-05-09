30db89000a974e9fe8fdbc6cde082f9cf7da251c
Yu Ho Chun <yuhappy134@gmail.com>
2025-05-09 15:24:29 +0800

    Update db_to_txt.py

--- db_to_txt.py
+++ db_to_txt.py
@@ -1,47 +1,73 @@
-import sqlite3
+def generate_spider_report():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    
+    with open('spider.txt', 'w', encoding='utf-8') as f:
+        c.execute('SELECT page_id, title, date, size FROM web_info')
+        pages = c.fetchall()
+        
+        for page in pages:
+            page_id, title, date, size = page
+            
+            # Get URL
+            c.execute('SELECT link FROM link_2_id WHERE link_id = ?', (page_id,))
+            url_row = c.fetchone()
+            url = url_row[0] if url_row else "URL not found"
+            
+            # Get keywords with frequencies
+            c.execute('''
+                SELECT k.keyword, SUM(f.frequency) 
+                FROM forward_idx f
+                JOIN keyword_2_id k ON f.keyword_id = k.keyword_id
+                WHERE f.page_id = ?
+                GROUP BY k.keyword
+            ''', (page_id,))
+            keywords = [f"{row[0]} {row[1]}" for row in c.fetchall()]
+            
+            # Get parent links
+            c.execute('''
+                SELECT l.link 
+                FROM parent_child p
+                JOIN link_2_id l ON p.parent_id = l.link_id
+                WHERE p.child_id = ?
+            ''', (page_id,))
+            parents = [row[0] for row in c.fetchall()]
+            
+            # Get child links
+            c.execute('''
+                SELECT l.link 
+                FROM parent_child p
+                JOIN link_2_id l ON p.child_id = l.link_id
+                WHERE p.parent_id = ?
+            ''', (page_id,))
+            children = [row[0] for row in c.fetchall()]
+            
+            # Write to file
+            f.write(f"score {page_id} {title}\n")
+            f.write(f"{url}\n")
+            f.write(f"{date}, {size} bytes\n")
+            f.write("; ".join(keywords) + "\n\n")
+            
+            f.write("Parents:\n")
+            if parents:
+                for parent in parents:
+                    f.write(f"{parent}\n")
+            else:
+                f.write("No parents\n")
+            
+            f.write("\nChildren:\n")
+            if children:
+                for child in children:
+                    f.write(f"{child}\n")
+            else:
+                f.write("No children\n")
+            
+            f.write(f"\nscore {page_id} {title}\n")
+            f.write("=" * 80 + "\n\n")
+    
+    conn.close()
+    print("Report generated as spider.txt")
 
-connection = sqlite3.connect('database.db')
-cursor = connection.cursor()
-
-cursor.execute(f'SELECT COUNT(*) FROM web_info')
-number_of_page = cursor.fetchone()[0]
-
-with open('spider_result.txt', 'w', encoding='utf-8') as file:
-    for link_id in range(1, number_of_page+1):
-        # background info
-        cursor.execute(f'SELECT * FROM web_info WHERE page_id = {link_id}')
-        _, title, date, size = cursor.fetchone()
-        cursor.execute(f'SELECT * FROM link_2_id WHERE link_id = {link_id}')
-        _, url = cursor.fetchone()
-        file.write(title + '\n')
-        file.write(url + '\n')
-        file.write(date + ', ' + str(size) + '\n')
-
-        # keyword info
-        cursor.execute(f'SELECT * FROM forward_idx WHERE page_id = {link_id}')
-        rows = cursor.fetchall()
-        rows = sorted(rows, key=lambda x: x[2], reverse=True)
-        used_word_id = []
-        row_num = 0
-        while len(used_word_id) < 100 and row_num < len(rows):
-            _, keyword_id, frequency, posiiton = rows[row_num]
-            if keyword_id not in used_word_id:
-                used_word_id.append(keyword_id)
-                cursor.execute(f'SELECT * FROM keyword_2_id WHERE keyword_id = {keyword_id}')
-                _, keyword = cursor.fetchone()
-                file.write(keyword + ' ' + str(frequency) + '; ')
-            row_num += 1
-        file.write('\n')
-
-        # child info
-        cursor.execute(f'SELECT * FROM parent_child WHERE parent_id = {link_id}')
-        rows = cursor.fetchall()
-        for row in rows[:10]:
-            _, child_id = row
-            cursor.execute(f'SELECT * FROM link_2_id WHERE link_id = {child_id}')
-            _, url = cursor.fetchone()
-            file.write(url + '\n')
-
-        file.write('----------\n')
-
+# Run the report generation
+generate_spider_report()
 print('txt generated')
